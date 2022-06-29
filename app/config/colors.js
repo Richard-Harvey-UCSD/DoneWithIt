f558106ee2c048e6db3b426c8898973ef943f2fc
Richard Harvey <theerichardharvey@gmail.com>
2022-06-29 17:32:52 -0400

    Building the ListItem Component

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,7 @@
 export default {
+  black: '#000000',
+  medium: '#6e6969',
   primary: '#fc5c65',
   secondary: '#4ecdc4',
-  black: '#000000',
   white: '#ffffff',
 };
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 
 import AppText from '../components/AppText';
+import ListItem from '../components/ListItem';
 import colors from '../config/colors';
 
 
@@ -12,6 +13,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/mosh.jpg')}
+            title='Mosh Hamedani'
+            subTitle='5 Listings'
+          />
+        </View>
       </View>
     </View>
   );
@@ -35,6 +43,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '500',
   },
+  userContainer: {
+    marginVertical: 40,
+  }
 });
 
 export default ListingDetailsScreen;
