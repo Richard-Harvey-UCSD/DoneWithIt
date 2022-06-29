f180a5535686f1df2be91758c0739559c64c4ce7
Richard Harvey <theerichardharvey@gmail.com>
2022-06-29 12:09:14 -0400

    Building the Listing Details Screen

--- App.js
+++ App.js
@@ -18,19 +18,10 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import AppText from './app/components/AppText';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return <View style={{
-    backgroundColor: '#f8f4f4',
-    padding: 20,
-    paddingTop: 100,
-  }}>
-    <Card
-      title='Red jacket for sale'
-      subTitle='$100'
-      image={require('./app/assets/jacket.jpg')}
-    />
-  </View>;
+  return <ListingDetailsScreen />;
 }
 
 const styles = StyleSheet.create({
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+import AppText from '../components/AppText';
+import colors from '../config/colors';
+
+
+function ListingDetailsScreen(props) {
+  return (
+    <View>
+      <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+});
+
+export default ListingDetailsScreen;
