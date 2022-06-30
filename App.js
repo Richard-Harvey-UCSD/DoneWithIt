139bc719d97f90d5567a8db760b5843e512b2336
Richard Harvey <theerichardharvey@gmail.com>
2022-06-30 08:07:55 -0400

    Improving the View Image Screen

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Platform, StyleSheet, Text } from 'react-native';
 
-import styles from './AppText/styles';
+// import styles from './AppText/styles';
 
 // <Heading>My Heading</Heading>
 function AppText({ children, style }) {
@@ -9,5 +9,21 @@ function AppText({ children, style }) {
     <Text style={[styles.text, style]}>{children}</Text>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: 'Avenir',
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: 'Roboto',
+      },
+    }),
+  },
+});
 
 export default AppText;
