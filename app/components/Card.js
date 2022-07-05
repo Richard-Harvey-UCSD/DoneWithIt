de38ef8c7792a2b1d0e886c72095eae35f3e5b3f
Richard Harvey <theerichardharvey@gmail.com>
2022-07-05 12:13:28 -0400

    Fetching Data

--- app/components/Card.js
+++ app/components/Card.js
@@ -4,11 +4,11 @@ import { Image, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-n
 import colors from '../config/colors';
 import AppText from './AppText';
 
-function Card({ title, subTitle, image, onPress }) {
+function Card({ title, subTitle, imageUrl, onPress }) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={image} />
+        <Image style={styles.image} source={{ uri: imageUrl }} />
         <View style={styles.detailsContainer}>
           <AppText style={styles.title} numberOfLines={1}>{title}</AppText>
           <AppText style={styles.subTitle} numberOfLines={2}>{subTitle}</AppText>
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Platform, PlatformColor, StyleSheet } from 'react-native';
-import Card from '../components/Card';
 
+
+import Card from '../components/Card';
 import colors from '../config/colors';
+import listingsApi from '../api/listings';
 import routes from '../navigation/routes';
 import Screen from '../components/Screen';
 
-const listings = [
-  {
-    id: 1,
-    title: 'Red jacket for sale',
-    price: 100,
-    image: require('../assets/jacket.jpg')
-  },
-  {
-    id: 2,
-    title: 'Couch in great condition',
-    price: 1000,
-    image: require('../assets/couch.jpg')
-  },
-];
+// const listings = [
+//   {
+//     id: 1,
+//     title: 'Red jacket for sale',
+//     price: 100,
+//     image: require('../assets/jacket.jpg')
+//   },
+//   {
+//     id: 2,
+//     title: 'Couch in great condition',
+//     price: 1000,
+//     image: require('../assets/couch.jpg')
+//   },
+// ];
 
 function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -31,7 +44,7 @@ function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={'$' + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         }
