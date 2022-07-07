1366b98bd190f4742ae6990591f772af59145041
Richard Harvey <theerichardharvey@gmail.com>
2022-07-07 13:33:37 -0400

    Implementing a Cache Layer

--- app/api/client.js
+++ app/api/client.js
@@ -1,5 +1,7 @@
 import { create } from 'apisauce';
 
+import cache from '../utility/cache';
+
 const apiClient = create({
   baseURL: 'http://192.168.1.210:9000/api'
 });
@@ -9,5 +11,20 @@ const apiClient = create({
 //     response.problem;
 //   }
 // });
+
+const get = apiClient.get;
+apiClient.get = async (url, params, axiosConfig) => {
+  // before
+  const response = await get(url, params, axiosConfig);
+
+  if (response.ok) {
+    cache.store(url, response.data);
+    return response;
+  }
+
+  const data = await cache.get(url);
+  return data ? { ok: true, data } : response;
+  // after
+};
 
 export default apiClient;
