df3bd53a61376a4385a261ae261e0e80d458305c
Richard Harvey <theerichardharvey@gmail.com>
2022-07-08 19:11:43 -0400

    Adding an Overlay

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
+import { StyleSheet, View } from 'react-native';
 
 function ActivityIndicator({ visible = false }) {
   if (!visible) return null;
 
   return (
-    <LottieView
-      autoPlay
-      loop
-      source={require('../assets/animations/loader.json')}
-    />
+    <View style={styles.overlay}>
+      <LottieView
+        autoPlay
+        loop
+        source={require('../assets/animations/loader.json')}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  overlay: {
+    backgroundColor: 'white',
+    height: '100%',
+    opacity: 0.8,
+    position: 'absolute',
+    width: '100%',
+    zIndex: 1,
+  },
+});
 
 export default ActivityIndicator;
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -62,28 +62,30 @@ function ListingsScreen({ navigation }) {
 
 
   return (
-    <Screen style={styles.screen}>
-      {error && (
-        <>
-          <AppText>Couldn't retrieve the listings.</AppText>
-          <AppButton title='Retry' onPress={loadListings} />
-        </>
-      )}
+    <>
       <ActivityIndicator visible={loading} />
-      <FlatList
-        data={listings}
-        keyExtractor={listing => listing.id.toString()}
-        renderItem={({ item }) =>
-          <Card
-            title={item.title}
-            subTitle={'$' + item.price}
-            imageUrl={item.images[0].url}
-            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-            thumbnailUrl={item.images[0].thumbnailUrl}
-          />
-        }
-      />
-    </Screen>
+      <Screen style={styles.screen}>
+        {error && (
+          <>
+            <AppText>Couldn't retrieve the listings.</AppText>
+            <AppButton title='Retry' onPress={loadListings} />
+          </>
+        )}
+        <FlatList
+          data={listings}
+          keyExtractor={listing => listing.id.toString()}
+          renderItem={({ item }) =>
+            <Card
+              title={item.title}
+              subTitle={'$' + item.price}
+              imageUrl={item.images[0].url}
+              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+              thumbnailUrl={item.images[0].thumbnailUrl}
+            />
+          }
+        />
+      </Screen>
+    </>
   );
 }
 
